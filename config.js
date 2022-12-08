96240679c40a96a35e2362c38bb5648dc73998e7
dhanushvarman <dhanushvarmanj@gmail.com>
2022-12-08 18:33:53 +0530

    copy

--- config.js
+++ config.js
@@ -1,23 +1,26 @@
 var mongodb = require('mongodb');
+var dotenv = require('dotenv').config();
 var mongoClient = mongodb.MongoClient;
-const dotenv = require("dotenv").config();
 let db;
 let connection;
 
-async function connectDb(req, res, next) {
+async function connectDb(req,res,next){
+    try {
+        connection = await mongoClient.connect(process.env.DB);
+        db = await connection.db("Reset")
+        return db
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message:"Something Went Wrong"})
+    }
+} 
 
-    connection = await mongoClient.connect(process.env.DB);
-    db = await connection.db("Reset");
-    return db
-}
-
-async function closeConnection(req, res, next) {
-    if (connection) {
+async function closeConnection(req,res,next){
+    if(connection){
         await connection.close()
-    } else {
+    }else{
         console.log("No Connection")
     }
-
 }
 
-module.exports = { connectDb, connection, db, closeConnection }
+module.exports = {connectDb,closeConnection,db,connection}
